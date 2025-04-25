5b45d833a59f47257868805fae9ede25a53c1ba9
rejzzzz <hoquerejwanulrh@gmail.com>
2025-04-26 02:35:01 +0530

    the build process has some error, needs to be fixed

---
+++ build/CMakeFiles/TradingApp.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/codebind/cpp/deribit-system/latency.cpp" "CMakeFiles/TradingApp.dir/latency.cpp.o" "gcc" "CMakeFiles/TradingApp.dir/latency.cpp.o.d"
+  "/home/codebind/cpp/deribit-system/main.cpp" "CMakeFiles/TradingApp.dir/main.cpp.o" "gcc" "CMakeFiles/TradingApp.dir/main.cpp.o.d"
+  "/home/codebind/cpp/deribit-system/threads.cpp" "CMakeFiles/TradingApp.dir/threads.cpp.o" "gcc" "CMakeFiles/TradingApp.dir/threads.cpp.o.d"
+  "/home/codebind/cpp/deribit-system/trade.cpp" "CMakeFiles/TradingApp.dir/trade.cpp.o" "gcc" "CMakeFiles/TradingApp.dir/trade.cpp.o.d"
+  "/home/codebind/cpp/deribit-system/websocket.cpp" "CMakeFiles/TradingApp.dir/websocket.cpp.o" "gcc" "CMakeFiles/TradingApp.dir/websocket.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
